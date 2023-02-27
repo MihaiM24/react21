7328503704a237b35b4895fdeb62ee289bde591d
Mihai Mitricel <mihaimitricel24@gmail.com>
2023-02-27 14:26:09 +0200

    ex 21

---
+++ src/Counter.js
@@ -0,0 +1,21 @@
+import React from "react";
+export class Counter extends React.Component{
+    state ={
+        count: this.props.initialValue ?? 0
+    }
+    constructor(props){
+        super(props);
+        setInterval(() =>{
+            this.setState((state) =>{
+                return (
+                    {
+                        count: state.count + (this.props.incrementBy ?? 1)
+                    }
+                )
+            })
+        },this.props.timeOut ?? 1000)
+    }
+    render(){
+        return (<div><h1> {this.state.count} </h1></div>)
+    }
+}
